60751611e5d5a4a83869a19c255b3ae4e3da914c
Zac Scott <zac.scott11@gmail.com>
2022-06-11 06:49:12 +1200

    Add @_terminal decorator
    This provides a distinction between actions that
    mutate the stream and actions that close it.

--- python_streams/steams.py
+++ python_streams/steams.py
@@ -5,12 +5,23 @@ from typing import Iterable, TypeVar, Iterator, List, Callable
 T = TypeVar("T")
 
 
-def _open(func):
+def _operation(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         self: Stream = args[0]
         if not self.open:
             raise RuntimeError("The stream has been closed")
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def _terminal(func):
+    @functools.wraps(func)
+    @_operation
+    def wrapper(*args, **kwargs):
+        self: Stream = args[0]
+        self.open = False
         return func(*args, **kwargs)
 
     return wrapper
@@ -32,12 +43,11 @@ class Stream:
             warnings.warn("The stream was not closed", RuntimeWarning)
         self.open = False
 
-    @_open
+    @_terminal
     def to_list(self) -> List[T]:
-        self.open = False
         return list(self.iterable)
 
-    @_open
+    @_operation
     def apply(self, f: Callable[[T], T]) -> "Stream":
         self.iterable = (f(t) for t in self.iterable)
         return self
